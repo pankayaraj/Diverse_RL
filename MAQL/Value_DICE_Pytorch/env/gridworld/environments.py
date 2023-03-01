ce5a9cca156cfd0c3ba20b1d1a460122e45e2b54
Pankayaraj <pankayaraj1995@gmail.com>
2023-03-01 12:49:27 -0500

    random

--- MAQL/Value_DICE_Pytorch/env/gridworld/environments.py
+++ MAQL/Value_DICE_Pytorch/env/gridworld/environments.py
@@ -46,7 +46,7 @@ class GridWalk(object):
     self._n_state = length ** 2
     self._n_action = 5
     self._target_x = length - 1
-    self._target_y = length - 1
+    self._target_y = 0
     self.discrete_action = True
     self.current_state = np.array([0, 0])
     self.traget_state = np.array([self._target_x, self._target_y])
---
+++ MAQL/Value_DICE_Pytorch/learn_q_function.py
@@ -0,0 +1,52 @@
+import torch
+import numpy as np
+import sys
+from q_learning.q_learning import Q_learning
+from env.gridworld.environments import GridWalk
+from model import NN_Paramters
+from parameters import Algo_Param, Save_Paths, Load_Paths
+
+
+q_param = NN_Paramters(state_dim=2, action_dim=5, hidden_layer_dim=[10, 10], non_linearity=torch.tanh, device=torch.device("cpu"), l_r=0.05)
+algo_param = Algo_Param()
+algo_param.gamma = 0.9
+
+max_episodes = 100
+
+grid_size = 10
+
+env = GridWalk(grid_size, False)
+env2 = GridWalk(grid_size, False)
+
+Q = Q_learning(env, q_param, algo_param)
+#Q.load("q", "target_q")
+update_interval = 10
+save_interval = 1000
+eval_interval = 1000
+state = Q.initalize()
+
+for i in range(10000):
+
+    Q.train()
+    state = Q.step(state)
+    if i%update_interval == 0:
+        Q.hard_update()
+    if i%save_interval == 0:
+        print("saving")
+        Q.save("qy0", "target_qy0")
+    if i%eval_interval == 0:
+        s = env2.reset()
+        i_s = s
+        rew = 0
+        for j in range(max_episodes):
+            a = Q.get_action(s)
+            s, r, d, _ = env2.step(a)
+            rew += r
+            if j == max_episodes-1:
+                d = True
+            if d == True:
+                break
+        print("reward at itr " + str(i) + " = " + str(rew) + " at state: " + str(s) + " starting from: " + str(i_s))
+#Q.memory = torch.load("mem")
+#torch.save(Q.memory, "mem")
+
