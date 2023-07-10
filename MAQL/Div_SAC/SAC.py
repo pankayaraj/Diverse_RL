b00e5a0d08c3dd35a12e42a3e1c9550ce86e2fca
Pankayaraj <pankayaraj1995@gmail.com>
2023-07-10 09:06:43 -0400

    ran

--- MAQL/Div_SAC/SAC.py
+++ MAQL/Div_SAC/SAC.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 
 
-from model import Q_Function_NN, Continuous_Gaussian_Policy
+from model import Q_Function_Z_NN, Continuous_Gaussian_Policy
 from memory import Replay_Memory
 
 
@@ -18,7 +18,7 @@ class SAC():
         self.env = env
         self.env_type = env_type
         self.device = q_nn_param.device
-
+        self.inital_state = None
 
         self.num_z = num_z
 
@@ -58,21 +58,21 @@ class SAC():
 
         for i in range(self.num_z):
 
-            self.critic_1[i] = Q_Function_NN(nn_params=q_nn_param, save_path=save_path.q_path, load_path=load_path.q_path)
-            self.critic_2[i] = Q_Function_NN(nn_params=q_nn_param, save_path=save_path.q_path, load_path=load_path.q_path)
+            self.critic_1[i] = Q_Function_Z_NN(nn_params=q_nn_param, save_path=save_path.q_path, load_path=load_path.q_path)
+            self.critic_2[i] = Q_Function_Z_NN(nn_params=q_nn_param, save_path=save_path.q_path, load_path=load_path.q_path)
 
-            self.critic_target_1[i] = Q_Function_NN(nn_params=q_nn_param, save_path=save_path.q_path, load_path=load_path.q_path)
-            self.critic_target_2[i] = Q_Function_NN(nn_params=q_nn_param, save_path=save_path.q_path, load_path=load_path.q_path)
+            self.critic_target_1[i] = Q_Function_Z_NN(nn_params=q_nn_param, save_path=save_path.q_path, load_path=load_path.q_path)
+            self.critic_target_2[i] = Q_Function_Z_NN(nn_params=q_nn_param, save_path=save_path.q_path, load_path=load_path.q_path)
 
             self.policy[i] = Continuous_Gaussian_Policy(policy_nn_param, save_path=save_path.policy_path,
                                                         load_path=load_path.policy_path, action_space=action_space)
 
-            self.critic_target_1[i].load_state_dict(self.critic_1.state_dict())
-            self.critic_target_2[i].load_state_dict(self.critic_2.state_dict())
+            self.critic_target_1[i].load_state_dict(self.critic_1[i].state_dict())
+            self.critic_target_2[i].load_state_dict(self.critic_2[i].state_dict())
 
-            self.critic_1_optim[i] = torch.optim.Adam(self.critic_1.parameters(), self.q_nn_param.l_r)
-            self.critic_2_optim[i] = torch.optim.Adam(self.critic_2.parameters(), self.q_nn_param.l_r)
-            self.policy_optim[i] = torch.optim.Adam(self.policy.parameters(), self.q_nn_param.l_r)
+            self.critic_1_optim[i] = torch.optim.Adam(self.critic_1[i].parameters(), self.q_nn_param.l_r)
+            self.critic_2_optim[i] = torch.optim.Adam(self.critic_2[i].parameters(), self.q_nn_param.l_r)
+            self.policy_optim[i] = torch.optim.Adam(self.policy[i].parameters(), self.q_nn_param.l_r)
 
             if self.automatic_alpha_tuning is True:
                 self.target_entropy[i] = -torch.prod(torch.Tensor(self.env.action_space.shape).to(self.device)).item()
@@ -81,10 +81,13 @@ class SAC():
 
         
         self.replay_buffer = Replay_Memory(capacity=memory_capacity)
+
+        self.current_z_index = 0
+        self.t = 0
         
     def get_action(self, state, evaluate=False):
 
-        action, log_prob, action_mean = self.policy.sample(state, format="torch")
+        action, log_prob, action_mean = self.policy[self.current_z_index].sample(state, format="torch")
 
         if evaluate == False:
             return action.cpu().detach().numpy(), action_mean.cpu().detach().numpy()
@@ -97,8 +100,12 @@ class SAC():
         self.steps_done = 0
         self.steps_per_eps = 0
         state = self.env.reset()
+
+        self.inital_state = state
+        self.t +=1
         for i in range(self.batch_size):
             state = self.step(state)
+            self.t += 1
         return state
 
     def update(self, batch_size=None):
@@ -121,58 +128,58 @@ class SAC():
 
 
         with torch.no_grad():
-            next_action_batch, next_log_prob_batch, _ = self.policy.sample(next_state_batch, format="torch")
-            q1_next_target = self.critic_target_1.get_value(next_state_batch, next_action_batch, format="torch")
-            q2_next_target = self.critic_target_2.get_value(next_state_batch, next_action_batch, format="torch")
+            next_action_batch, next_log_prob_batch, _ = self.policy[self.current_z_index].sample(next_state_batch, format="torch")
+            q1_next_target = self.critic_target_1[self.current_z_index].get_value(next_state_batch, next_action_batch, format="torch")
+            q2_next_target = self.critic_target_2[self.current_z_index].get_value(next_state_batch, next_action_batch, format="torch")
             min_q_target = torch.min(q1_next_target, q2_next_target) - self.alpha*next_log_prob_batch
             next_q_value = reward_batch + done_mask_batch*self.gamma*min_q_target
 
-        q1 = self.critic_1.get_value(state_batch, action_batch)
-        q2 = self.critic_2.get_value(state_batch, action_batch)
+        q1 = self.critic_1[self.current_z_index].get_value(state_batch, action_batch)
+        q2 = self.critic_2[self.current_z_index].get_value(state_batch, action_batch)
 
         q1_loss = 0.5*torch.nn.functional.mse_loss(q1, next_q_value)
         q2_loss = 0.5*torch.nn.functional.mse_loss(q2, next_q_value)
 
 
-        self.critic_1_optim.zero_grad()
+        self.critic_1_optim[self.current_z_index].zero_grad()
         q1_loss.backward()
-        self.critic_1_optim.step()
+        self.critic_1_optim[self.current_z_index].step()
 
-        self.critic_2_optim.zero_grad()
+        self.critic_2_optim[self.current_z_index].zero_grad()
         q2_loss.backward()
-        self.critic_2_optim.step()
+        self.critic_2_optim[self.current_z_index].step()
 
 
-        pi, log_pi, _ = self.policy.sample(state_batch)
+        pi, log_pi, _ = self.policy[self.current_z_index].sample(state_batch)
 
 
         if self.automatic_alpha_tuning:
             alpha_loss = -(self.log_alpha * (log_pi + self.target_entropy).detach()).mean()
-            self.alpha_optim.zero_grad()
+            self.alpha_optim[self.current_z_index].zero_grad()
             alpha_loss.backward()
-            self.alpha_optim.step()
+            self.alpha_optim[self.current_z_index].step()
 
-            self.alpha = self.log_alpha.exp().detach()
+            self.alpha = self.log_alpha[self.current_z_index].exp().detach()
 
 
-        q1_pi = self.critic_1.get_value(state_batch, pi)
-        q2_pi = self.critic_2.get_value(state_batch, pi)
+        q1_pi = self.critic_1[self.current_z_index].get_value(state_batch, pi)
+        q2_pi = self.critic_2[self.current_z_index].get_value(state_batch, pi)
         min_q_pi = torch.min(q1_pi, q2_pi)
 
 
 
         policy_loss = ((self.alpha*log_pi) - min_q_pi).mean()
 
-        self.policy_optim.zero_grad()
+        self.policy_optim[self.current_z_index].zero_grad()
         policy_loss.backward()
-        self.policy_optim.step()
+        self.policy_optim[self.current_z_index].step()
 
 
 
         if self.update_no%self.target_update_interval == 0:
 
-            self.soft_update(self.critic_target_1, self.critic_1, self.tau)
-            self.soft_update(self.critic_target_2, self.critic_2, self.tau)
+            self.soft_update(self.critic_target_1[self.current_z_index], self.critic_1[self.current_z_index], self.tau)
+            self.soft_update(self.critic_target_2[self.current_z_index], self.critic_2[self.current_z_index], self.tau)
 
 
     def step(self, state, random=False):
@@ -185,31 +192,31 @@ class SAC():
             action, action_mean = self.get_action(state, evaluate=False)
 
         next_state, reward, done, _ = self.env.step(action)
-
+        self.t += 1
         self.steps_done += 1
         self.steps_per_eps += 1
 
         if done:
             mask = 0.0
-            self.replay_buffer.push(state, action, action_mean, reward, next_state, mask)
+            self.replay_buffer.push(state, action, action_mean, reward, next_state, self.inital_state, mask, self.t, False)
             next_state = self.env.reset()
             self.steps_per_eps = 0
             return next_state
 
         if self.steps_per_eps == self.max_episodes:
             mask = 1.0
-            self.replay_buffer.push(state, action, action_mean, reward, next_state, mask)
+            self.replay_buffer.push(state, action, action_mean, reward, next_state, self.inital_state, mask, self.t, False)
             next_state = self.env.reset()
             self.steps_per_eps = 0
             return next_state
         mask = 1.0
 
-        self.replay_buffer.push(state, action, action_mean, reward, next_state, mask)
+        self.replay_buffer.push(state, action, action_mean, reward, next_state, self.inital_state, mask, self.t, False)
         return next_state
 
     def hard_update(self):
-        self.critic_target_1.load_state_dict(self.critic_1.state_dict())
-        self.critic_target_2.load_state_dict(self.critic_2.state_dict())
+        self.critic_target_1[self.current_z_index].load_state_dict(self.critic_1[self.current_z_index].state_dict())
+        self.critic_target_2[self.current_z_index].load_state_dict(self.critic_2[self.current_z_index].state_dict())
 
     def soft_update(self, target, source, tau):
         for target_param, param in zip(target.parameters(), source.parameters()):
@@ -219,18 +226,18 @@ class SAC():
              critic_1_target_path = "critic_1_target", critic_2_target_path = "critic_2_target",
              policy_path= "policy_target"):
 
-        self.critic_1.save(critic_1_path)
-        self.critic_2.save(critic_2_path)
-        self.critic_target_1.save(critic_1_target_path)
-        self.critic_target_2.save(critic_2_target_path)
-        self.policy.save(policy_path)
+        self.critic_1[self.current_z_index].save(critic_1_path)
+        self.critic_2[self.current_z_index].save(critic_2_path)
+        self.critic_target_1[self.current_z_index].save(critic_1_target_path)
+        self.critic_target_2[self.current_z_index].save(critic_2_target_path)
+        self.policy[self.current_z_index].save(policy_path)
 
     def load(self, critic_1_path="critic_1", critic_2_path="critic_2",
              critic_1_target_path = "critic_1_target", critic_2_target_path = "critic_2_target",
              policy_path= "policy_target"):
 
-        self.critic_1.load(critic_1_path)
-        self.critic_2.load(critic_2_path)
-        self.critic_target_1.load(critic_1_target_path)
-        self.critic_target_2.load(critic_2_target_path)
-        self.policy.load(policy_path)
+        self.critic_1[self.current_z_index].load(critic_1_path)
+        self.critic_2[self.current_z_index].load(critic_2_path)
+        self.critic_target_1[self.current_z_index].load(critic_1_target_path)
+        self.critic_target_2[self.current_z_index].load(critic_2_target_path)
+        self.policy[self.current_z_index].load(policy_path)
--- MAQL/Div_SAC/memory.py
+++ MAQL/Div_SAC/memory.py
@@ -3,21 +3,23 @@ import numpy as np
 
 class Transition_tuple():
 
-    def __init__(self, state, action, reward, next_state,
-                 inital_state, time_step, optim_traj
+    def __init__(self, state, action, action_mean, reward, next_state,
+                 inital_state, done, time_step, optim_traj
                  ):
         #expects as list of items for each initalization variable
         self.state = np.array(state)
         self.action = np.array(action)
+        self.action_mean = np.array(action_mean)
         self.reward = np.array(reward)
         self.next_state = np.array(next_state)
         self.initial_state = np.array(inital_state)
+        self.done = np.array(done)
         self.time_step = np.array(time_step)
         self.optim_traj = np.array(optim_traj)
 
 
     def get_all_attributes(self):
-        return [self.state, self.action, self.reward, self.next_state, self.initial_state, self.time_step, self.optim_traj]
+        return [self.state, self.action, self.action_mean, self.reward, self.next_state, self.initial_state, self.done, self.time_step, self.optim_traj]
 
 class Replay_Memory():
 
@@ -25,15 +27,17 @@ class Replay_Memory():
         self.no_data = 0
         self.position = 0
         self.capacity = capacity
-        self.state, self.action, self.reward, self.next_state, self.inital_state, self.time_step, self.optim_traj = [], [], [], [], [], [], []
+        self.state, self.action, self.action_mean, self.reward, self.next_state, self.inital_state, self.done, self.time_step, self.optim_traj = [], [], [], [], [], [], [], [], []
 
-    def push(self, state, action, reward, next_state, inital_state, time_step, optim_traj):
+    def push(self, state, action, action_mean, reward, next_state, inital_state, done, time_step, optim_traj):
         if len(self.state) < self.capacity:
             self.state.append(None)
             self.action.append(None)
+            self.action_mean.append(None)
             self.reward.append(None)
             self.next_state.append(None)
             self.inital_state.append(None)
+            self.done.append(None)
             self.time_step.append(None)
             self.optim_traj.append(None)
             self.no_data += 1
@@ -41,9 +45,11 @@ class Replay_Memory():
 
         self.state[self.position] = state
         self.action[self.position] = action
+        self.action_mean[self.position] = action_mean
         self.reward[self.position] = reward
         self.next_state[self.position] = next_state
         self.inital_state[self.position] = inital_state
+        self.done[self.position] = done
         self.time_step[self.position] = time_step
         self.optim_traj[self.position] = optim_traj
 
@@ -60,14 +66,16 @@ class Replay_Memory():
 
         state = np.take(np.array(self.state), indices, axis=0)
         action = np.take(np.array(self.action), indices, axis=0)
+        action_mean = np.take(np.array(self.action_mean), indices, axis=0)
         reward = np.take(np.array(self.reward), indices, axis=0)
         next_state = np.take(np.array(self.next_state), indices, axis=0)
         initial_state = np.take(np.array(self.inital_state), indices, axis=0)
+        done = np.take(np.array(self.done), indices, axis=0)
         time_step = np.take(np.array(self.time_step), indices, axis=0)
         optim_traj = np.take(np.array(self.optim_traj), indices, axis=0)
 
 
-        return Transition_tuple(state, action, reward, next_state, initial_state, time_step, optim_traj)
+        return Transition_tuple(state, action, action_mean, reward, next_state, initial_state, done, time_step, optim_traj)
 
     def iterate_through(self):
         all_data = self.sample(self.no_data)
@@ -89,24 +97,28 @@ def combine_transition_tuples(tuples):
 
     state = np.array([])
     action = np.array([])
+    action_mean = np.array([])
     reward = np.array([])
     next_state = np.array([])
     initial_state = np.array([])
+    done = np.array([])
     time_step = np.array([])
     optim_traj = np.array([])
 
     i = 0
     for tuple in tuples:
 
-        s, a, r, n_s, i_s, t, o_t = tuple.state, tuple.action, tuple.reward, tuple.next_state, tuple.initial_state, tuple.time_step, tuple.optim_traj
+        s, a, a_m, r, n_s, i_s, d, t, o_t = tuple.state, tuple.action, tuple.action_mean, tuple.reward, tuple.next_state, tuple.initial_state, tuple.done, tuple.time_step, tuple.optim_traj
         if i == 0:
 
 
             state = s
             action = a
+            action_mean = a_m
             reward = r
             next_state = n_s
             initial_state = i_s
+            done = d
             time_step = t
             optim_traj = o_t
 
@@ -120,15 +132,17 @@ def combine_transition_tuples(tuples):
 
             state = np.concatenate((state, s), axis=0)
             action = np.concatenate((action, a), axis=0)
+            action_mean = np.concatenate((action_mean, a_m), axis=0)
             reward = np.concatenate((reward, r), axis=0)
             #next_state = np.concatenate((next_state, n_s), axis=0)
             initial_state = np.concatenate((initial_state, i_s), axis=0)
+            time_step = np.concatenate((done, d), axis=0)
             time_step = np.concatenate((time_step, t), axis=0)
             optim_traj = np.concatenate((optim_traj, o_t), axis=0)
 
         i += 1
 
-    main_tuple = Transition_tuple(state, action, reward, next_state, initial_state, time_step, optim_traj)
+    main_tuple = Transition_tuple(state, action, action_mean, reward, next_state, initial_state, done,  time_step, optim_traj)
     return main_tuple
 
 
